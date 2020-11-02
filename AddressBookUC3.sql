96b018e76bf19c0c5ac77789fff10e9dec42d27a
Shreya0207 <shanu.shreya270@gmail.com>
2020-11-02 19:15:35 +0530

    [Shreya]Add: Inserting values into address book table

---
+++ AddressBookUC3.sql
@@ -0,0 +1,4 @@
+insert into address_book values
+('Shreya','Singh','Kurud','Bhilai','Chhattisgarh',12345,'987654', 'abc123@gmail.com'),
+('Vivek','Singh','ShankarNagar','Raipur', 'Chhattisgarh',45612,'852963', 'xyz456@gmail.com'),
+('Shanu','Salvatore','IndiraNagar','Lucknow','UttarPradesh',10101,'789456', 'abcde123@gmail.com')
