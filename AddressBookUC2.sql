73baa7541db93b76762e63fcdd3a8425d3bb981f
Shreya0207 <shanu.shreya270@gmail.com>
2020-11-02 19:01:20 +0530

    [Shreya]Add: Creating the address book table

---
+++ AddressBookUC2.sql
@@ -0,0 +1,11 @@
+create table address_book
+(
+First_Name varchar(25) not null,
+Last_Name varchar(25) not null,
+address varchar(40) not null,
+city varchar(20) not null,
+state varchar(25) not null,
+zip int not null,
+phone_number int not null,
+email varchar(50) not null,
+)
