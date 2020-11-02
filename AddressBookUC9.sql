1c5277943f9eab8427421a79d1595a98f1b976f4
Shreya0207 <shanu.shreya270@gmail.com>
2020-11-02 19:50:13 +0530

    [Shreya]Add: Added the type as brother using first name

---
+++ AddressBookUC9.sql
@@ -0,0 +1,8 @@
+alter table address_book add Type varchar(30)
+alter table address_book add Name varchar(30)
+select * from address_book
+update address_book
+Set Type = 'Brother'
+where First_Name = 'Vivek'
+
+ 
